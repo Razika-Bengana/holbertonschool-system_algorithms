8b80f526b1db7cd48112eebfa5662ec3c67357b9
Razika-Bengana <5003@holbertonstudents.com>
2023-06-15 22:11:09 +0200

    function that checks if a binary tree is a valid Red-Black Tree

--- red_black_tree/1-rb_tree_is_valid.c
+++ red_black_tree/1-rb_tree_is_valid.c
@@ -1,101 +1,116 @@
 #include "rb_trees.h"
 
 /**
- * rb_tree_is_valid - function that checks if a binary tree is a valid
- * Red-Black Tree
- *
- * @tree: Pointer to the root node of the tree to check
- *
- * This function checks if the given binary tree satisfies the properties of a
- * Red-Black Tree:
- *   1. It is a binary search tree (BST).
- *   2. Every node has a color either red or black.
- *   3. The root of the tree is always black.
- *   4. There are no two adjacent red nodes (a red node cannot have a
- *      red parent or red child).
- *   5. Every path from the root to a NULL node has the same number of
- *      black nodes.
- *
- * Return: 1 if the tree is a valid Red-Black Tree, - 0 otherwise.
+ * rb_tree_is_valid - checks if a binary tree is a valid Red-Black Tree
+ * @tree: pointer to root node of the tree to check
+ * Return: 1 if tree is valid, otherwise 0
  */
-
 int rb_tree_is_valid(const rb_tree_t *tree)
 {
-	if (tree == NULL)
+	int height;
+
+	if (!tree)
 		return (0);
 
-	/* Check BST property */
-	if (!is_bst(tree))
+	if (tree->color != BLACK)
 		return (0);
 
-	/* Check Red-Black Tree properties */
-	if (!is_red_black(tree, NULL, 0))
+	if (!is_binary_search_tree(tree, 0, 0))
+		return (0);
+
+	if (!has_valid_colors(tree))
+		return (0);
+
+	height = calculate_height(tree);
+
+	if (!check_black_height(tree, height, 0))
 		return (0);
 
 	return (1);
 }
 
 /**
- * is_bst - Helper function to check if a tree is a binary search tree (BST)
- *
- * @node: Pointer to the current node
- *
- * Return: - 1 if the tree rooted at @node is a binary search tree,
- *         - 0 otherwise.
+ * is_binary_search_tree - checks if tree is a valid Binary Search Tree
+ * @tree: binary tree
+ * @min: min value in the left subtree
+ * @max: max value in the right subtree
+ * Return: 1 if valid BST, otherwise 0
  */
-
-int is_bst(const rb_tree_t *node)
+int is_binary_search_tree(const rb_tree_t *tree, int min, int max)
 {
-	if (node == NULL)
+	if (!tree)
 		return (1);
 
-	if (node->left != NULL && node->left->n >= node->n)
+	if ((min && tree->n < min) || (max && tree->n > max))
 		return (0);
 
-	if (node->right != NULL && node->right->n <= node->n)
-		return (0);
-
-	if (!is_bst(node->left) || !is_bst(node->right))
-		return (0);
-
-	return (1);
+	return (is_binary_search_tree(tree->left, 0, tree->n) &&
+		is_binary_search_tree(tree->right, tree->n, 0));
 }
 
 /**
- * is_red_black - Helper function to check Red-Black Tree properties
- *
- * @node: Pointer to the current node
- * @parent: Pointer to the parent of the current node
- * @black_count: Number of black nodes from the root to the current node
- *
- * Return: - 1 if the tree rooted at @node satisfies the Red-Black Tree
- *         properties, 0 otherwise.
+ * has_valid_colors - checks if RBT has valid colors
+ * @tree: binary tree
+ * Return: 1 if colors are valid, otherwise 0
  */
-
-int is_red_black(const rb_tree_t *node, const rb_tree_t *parent,
-		 int black_count)
+int has_valid_colors(const rb_tree_t *tree)
 {
-	if (node == NULL)
+	if (!tree)
+		return (1);
+
+	if (tree->color != RED && tree->color != BLACK)
+		return (0);
+
+	if (tree->color == RED)
 	{
-		/*
-		 * Check property 5:
-		 * Every path from root to a NULL node has the same number
-		 * of black nodes
-		 */
-		return (black_count == 0);
+		if (tree->left && tree->left->color == RED)
+			return (0);
+		if (tree->right && tree->right->color == RED)
+			return (0);
 	}
 
-	/* Check property 3: The root of the tree is always black */
-	if (parent == NULL && node->color != BLACK)
+	return (has_valid_colors(tree->left) && has_valid_colors(tree->right));
+}
+
+/**
+ * calculate_height - gets height of red-black tree
+ * @tree: binary tree
+ * Return: maximum number of black nodes between root and leaf
+ */
+int calculate_height(const rb_tree_t *tree)
+{
+	int height;
+
+	if (!tree)
 		return (0);
 
-	/* Check property 4: There are no two adjacent red nodes */
-	if (parent != NULL && parent->color == RED && node->color == RED)
-		return (0);
+	height = MAXIMUM(calculate_height(tree->left), calculate_height(tree->right));
 
-	/* Recursively check the left and right subtrees */
-	return (is_red_black(node->left, node, (node->color == BLACK)
-			     ? black_count + 1 : black_count) &&
-		is_red_black(node->right, node, (node->color == BLACK)
-			     ? black_count + 1 : black_count));
+	if (tree->color == BLACK)
+		height++;
+
+	return (height);
+}
+
+/**
+ * check_black_height - checks if the height of black nodes in RBT is valid
+ * @tree: tree to check
+ * @black_height: number of black nodes
+ * @height: current height
+ * Return: 1 if height is valid, otherwise 0
+ */
+int check_black_height(const rb_tree_t *tree, int black_height, int height)
+{
+	if (!tree)
+	{
+		if (height == black_height)
+			return (1);
+		return (0);
+	}
+
+	if (tree->color == BLACK)
+		height++;
+
+	return (check_black_height(tree->left, black_height, height) &&
+		check_black_height(tree->right, black_height, height));
 }
