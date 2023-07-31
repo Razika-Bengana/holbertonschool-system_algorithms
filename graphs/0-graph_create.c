ab737c079dd522b36d60dd17305184f1618447d0
Razika-Bengana <5003@holbertonstudents.com>
2023-07-31 21:59:11 +0200

    0. Create graph

---
+++ graphs/0-graph_create.c
@@ -0,0 +1,27 @@
+#include "graphs.h"
+
+/**
+ * graph_create - Creates an empty graph_t structure.
+ *
+ * Return: A pointer to the newly created graph_t structure on success,
+ * or NULL if memory allocation fails
+ */
+
+graph_t *graph_create(void)
+{
+/* Allocate memory for the graph_t structure */
+	graph_t *graph = (graph_t *)malloc(sizeof(graph_t));
+
+/* Check if memory allocation was successful */
+	if (graph)
+	{
+/* Initialize the graph fields */
+/* Number of vertices is set to 0 as the graph is empty */
+		graph->nb_vertices = 0;
+
+/* The adjacency linked list starts empty, no vertices yet */
+		graph->vertices = NULL;
+	}
+
+	return (graph);
+}
