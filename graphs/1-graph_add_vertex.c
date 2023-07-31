121518e5132246d2a32593fa759236766661728f
Razika-Bengana <5003@holbertonstudents.com>
2023-08-01 00:56:43 +0200

    1. Add a vertex

--- graphs/1-graph_add_vertex.c
+++ graphs/1-graph_add_vertex.c
@@ -15,8 +15,8 @@ vertex_t *graph_add_vertex(graph_t *graph, const char *str)
 	if (graph == NULL || str == NULL)
 		return (NULL);
 
-	vertex_t *current_vertex;
-	vertex_t *new_vertex;
+	vertex_t *current_vertex = NULL;
+	vertex_t *new_vertex = NULL;
 
 	for (current_vertex = graph->vertices; current_vertex != NULL;
 	     current_vertex = current_vertex->next)
