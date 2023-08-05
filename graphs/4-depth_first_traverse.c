e0bf7aa63e5c6426b94f6e3527082648d4a50aa4
Razika-Bengana <5003@holbertonstudents.com>
2023-08-06 00:40:25 +0200

    4. Depth-first traversal

--- graphs/4-depth_first_traverse.c
+++ graphs/4-depth_first_traverse.c
@@ -1,66 +1,85 @@
 #include "graphs.h"
 
 /**
- * dfs_helper - the helper function for depth-first traversal
+ * traverseVerticesRecursively - recursive function that travels through
+ * vertices in a graph
  *
- * @current_vertex: the current vertex being visited
- * @depth: the depth of the current vertex
- * @visited: array to keep track of visited vertices
- * @action: pointer to the function to be called for each visited vertex
- * @max_depth: pointer to the maximum depth encountered during traversal
+ * @vertex: the starting vertex to traverse from
+ * @visited_flags: an array indicating whether vertices have been visited
+ * @current_depth: the current depth level in traversal
+ * @vertex_action: a function to perform an action on each vertex
  *
- * Return:nothing (void)
+ * Return: the maximum depth reached during traversal
  */
 
-void dfs_helper(const vertex_t *current_vertex, size_t depth,
-		       int *visited, void (*action)
-		       (const vertex_t *v, size_t depth), size_t *max_depth)
+size_t traverseVerticesRecursively(vertex_t *vertex, char *visited_flags,
+				   size_t current_depth, void (*vertex_action)
+				   (const vertex_t *v, size_t depth))
 {
-	edge_t *current_edge = current_vertex->edges;
+	size_t current_depth_new = 0, max_depth = current_depth, flag = 0;
+	edge_t *edge_header = NULL;
 
-	visited[current_vertex->index] = 1;
+	if (!vertex)
+		return (current_depth - 1);
 
-	if (depth > *max_depth)
-		*max_depth = depth;
-
-	action(current_vertex, depth);
-
-	while (current_edge)
+	edge_header = vertex->edges;
+	while (edge_header)
 	{
-		if (!visited[current_edge->dest->index])
-			dfs_helper(current_edge->dest, depth + 1, visited,
-				   action, max_depth);
+		if (visited_flags[edge_header->dest->index] == 0)
+		{
+			vertex_action(edge_header->dest, current_depth);
+			visited_flags[edge_header->dest->index] = 1;
+			current_depth_new = traverseVerticesRecursively
+				(edge_header->dest, visited_flags,
+				 current_depth + 1, vertex_action);
 
-		current_edge = current_edge->next;
+			if (current_depth_new > max_depth)
+				max_depth = current_depth_new;
+			flag = 1;
+		}
+		edge_header = edge_header->next;
 	}
+	if (flag)
+		return (max_depth);
+	return (max_depth - 1);
 }
 
 /**
  * depth_first_traverse - program that traverses a graph using
- * depth-first algorithm
+ * depth-first search
  *
- * @graph: a pointer to the graph to traverse
- * @action: a pointer to the function to be called for each visited vertex
+ * @graph: the graph to traverse
+ * @action: a function to perform an action on each vertex along with its depth
  *
- * Return: the biggest vertex depth, or 0 on failure
+ * Return: the maximum depth reached during traversal
  */
 
-size_t depth_first_traverse(const graph_t *graph, void (*action)
-			    (const vertex_t *v, size_t depth))
+size_t depth_first_traverse(const graph_t *graph,
+			    void (*action)(const vertex_t *v, size_t depth))
 {
-	size_t max_depth = 0;
-	int *visited = calloc(graph->nb_vertices, sizeof(int));
+	vertex_t *current_vertex = NULL;
+	char visited_flags[2048] = {0};
+	size_t current_depth = 0, max_depth = 0;
 
 	if (!graph || !action)
 		return (0);
 
-/* Visited array to keep track of visited vertices */
+	current_vertex = graph->vertices;
 
-	if (!visited)
-		return (0);
+	while (current_vertex)
+	{
+		if (visited_flags[current_vertex->index] == 0)
+		{
+			action(current_vertex, 0);
+			visited_flags[current_vertex->index] = 1;
+			current_depth = traverseVerticesRecursively
+				(current_vertex, visited_flags, 1, action);
 
-	dfs_helper(graph->vertices, 0, visited, action, &max_depth);
-
-	free(visited);
+			if (current_depth > max_depth)
+				max_depth = current_depth;
+		}
+		current_vertex = current_vertex->next;
+		break;
+	}
 	return (max_depth);
 }
