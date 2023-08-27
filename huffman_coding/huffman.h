8fd6261d5ee47eefaeaa1daf7e213acb24528922
Razika-Bengana <5003@holbertonstudents.com>
2023-08-27 15:32:46 +0200

    Update the header file

--- huffman_coding/huffman.h
+++ huffman_coding/huffman.h
@@ -31,4 +31,8 @@ heap_t *huffman_priority_queue(char *data, size_t *freq, size_t size);
 int huffman_extract_and_insert(heap_t *priority_queue);
 
 
+/* task 8 */
+binary_tree_node_t *huffman_tree(char *data, size_t *freq, size_t size);
+
+
 #endif /* HUFFMAN_H */
