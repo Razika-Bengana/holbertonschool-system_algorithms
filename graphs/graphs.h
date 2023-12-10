82f1ea952b51eff4c622dd05099223379666f2fd
RazikaBengana <razikabengana@gmail.com>
2023-12-10 21:59:42 +0100

    Update header file

--- graphs/graphs.h
+++ graphs/graphs.h
@@ -6,6 +6,8 @@
 #include <stddef.h>
 #include <string.h>
 
+
+
 /**
  * enum edge_type_e - Enumerates the different types of
  * connection between two vertices
@@ -13,22 +15,29 @@
  * @UNIDIRECTIONAL: The connection is made only in one way
  * @BIDIRECTIONAL: The connection is made in two ways
  */
+
 typedef enum edge_type_e
 {
 	UNIDIRECTIONAL = 0,
 	BIDIRECTIONAL
 } edge_type_t;
 
+
+
 /* Define the structure temporarily for usage in the edge_t */
 typedef struct vertex_s vertex_t;
 
+
+
 /**
  * struct edge_s - Node in the linked list of edges for a given vertex
+ *
  * A single vertex can have many edges
  *
  * @dest: Pointer to the connected vertex
  * @next: Pointer to the next edge
  */
+
 typedef struct edge_s
 {
 	vertex_t    *dest;
@@ -36,17 +45,19 @@ typedef struct edge_s
 } edge_t;
 
 
+
 /**
  * struct vertex_s - Node in the linked list of vertices in the adjacency list
  *
  * @index: Index of the vertex in the adjacency list.
  * @content: Custom data stored in the vertex (here, a string)
- * @nb_edges: Number of conenctions with other vertices in the graph
+ * @nb_edges: Number of connections with other vertices in the graph
  * @edges: Pointer to the head node of the linked list of edges
  * @next: Pointer to the next vertex in the adjacency linked list
  *   This pointer points to another vertex in the graph, but it
  *   doesn't stand for an edge between the two vertices
  */
+
 struct vertex_s
 {
 	size_t      index;
@@ -56,18 +67,66 @@ struct vertex_s
 	struct vertex_s *next;
 };
 
+
+
 /**
  * struct graph_s - Representation of a graph
+ *
  * We use an adjacency linked list to represent our graph
  *
  * @nb_vertices: Number of vertices in our graph
  * @vertices: Pointer to the head node of our adjacency linked list
  */
+
 typedef struct graph_s
 {
 	size_t      nb_vertices;
 	vertex_t    *vertices;
 } graph_t;
+
+
+
+/**
+ * struct GraphQueueNode - Node structure for a queue in graph traversal
+ *
+ * This structure represents a single node in a queue used for graph traversal.
+ * Each node holds a vertex from the graph, the depth of that vertex in the
+ * traversal, and a pointer to the next node in the queue.
+ *
+ * @vertex: Pointer to the vertex associated with this node
+ * @depth: The depth of the vertex in the traversal from the starting point
+ * @next: Pointer to the next node in the queue
+ */
+
+typedef struct GraphQueueNode
+{
+    vertex_t *vertex;
+    size_t depth;
+    struct GraphQueueNode *next;
+} GraphQueueNode;
+
+
+
+/**
+ * struct GraphQueue - Queue structure for managing nodes in graph traversal
+ *
+ * This structure represents a queue used for breadth-first traversal of a graph.
+ * It keeps track of the front and rear of the queue (head and tail), as well as
+ * the total number of nodes currently in the queue. This queue is used to
+ * manage the order in which vertices are visited during the traversal.
+ *
+ * @head: Pointer to the first node in the queue
+ * @tail: Pointer to the last node in the queue
+ * @size: The number of nodes currently in the queue
+ */
+
+typedef struct GraphQueue
+{
+    GraphQueueNode *head;
+    GraphQueueNode *tail;
+    size_t size;
+} GraphQueue;
+
 
 
 void graph_display(const graph_t *graph);
@@ -94,6 +153,9 @@ size_t depth_first_traverse(const graph_t *graph, void (*action)
 			    (const vertex_t *v, size_t depth));
 
 /* task 5. Breadth-first traversal */
+void freeQueue(GraphQueue *queue);
+int pushToQueue(GraphQueue *queue, vertex_t *vertex, size_t depth);
+int popFromQueue(GraphQueue *queue, vertex_t **vertex, size_t *depth);
 size_t breadth_first_traverse(const graph_t *graph, void (*action)
 			      (const vertex_t *v, size_t depth));
 
