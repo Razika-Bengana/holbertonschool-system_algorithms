d90347c4c80eedcf7d81492da4a7f1392d0598f0
RazikaBengana <razikabengana@gmail.com>
2023-12-10 21:59:15 +0100

    5. Breadth-first traversal

--- graphs/5-breadth_first_traverse.c
+++ graphs/5-breadth_first_traverse.c
@@ -1,56 +1,175 @@
 #include "graphs.h"
 
 /**
- * breadth_first_traverse - proogram that performs breadth-first traversal
- * on a graph
+ * freeQueue - program that frees all the memory allocated for the queue and its nodes
  *
- * @graph: a pointer to the graph to traverse
- * @vertex_action: a pointer to the function to be applied to each vertex
+ * this function iterates through each node in the queue, freeing them individually,
+ * and finally frees the queue itself;
+ * it ensures that all the memory allocated for queue management is properly released
  *
- * Return: the depth of traversal
- **/
+ * @queue: a pointer to the 'GraphQueue' structure to be freed
+ *
+ * Return: nothing (void)
+ */
 
-size_t breadth_first_traverse(const graph_t *graph, void (*vertex_action)
-			      (const vertex_t *vertex, size_t depth))
+void freeQueue(GraphQueue *queue)
 {
-	size_t depth, current_round_size, next_round_size;
-	vertex_t **vertex_queue;
-	char *visited_vertices;
-	edge_t *edge;
-	int queue_front, vertex_index;
+	GraphQueueNode *currentNode, *tempNode;
 
-	if (!graph || !vertex_action || graph->nb_vertices == 0)
+	currentNode = queue->head;
+	while (currentNode)
+	{
+		tempNode = currentNode;
+		currentNode = currentNode->next;
+		free(tempNode);
+	}
+	free(queue);
+}
+
+
+
+/**
+ * pushToQueue - program that adds a new vertex to the queue
+ *
+ * this function creates a new 'GraphQueueNode', assigns the provided vertex and depth to it,
+ * and adds it to the end of the queue;
+ * it handles memory allocation for the new node and updates the queue's tail;
+ * in case of memory allocation failure, it ensures that the queue is properly freed
+ *
+ * @queue: a pointer to the GraphQueue where the vertex will be added
+ * @vertex: a pointer to the vertex to be added to the queue
+ * @depth: the depth level of the vertex in the graph traversal
+ *
+ * Return: 1 if the vertex is successfully added, 0 if an error occurs
+ */
+
+int pushToQueue(GraphQueue *queue, vertex_t *vertex, size_t depth)
+{
+	GraphQueueNode *newNode;
+
+	newNode = calloc(1, sizeof(GraphQueueNode));
+	if (!newNode)
+	{
+		freeQueue(queue);
+		return (0);
+	}
+	newNode->vertex = vertex;
+	newNode->depth = depth;
+	if (!queue->tail)
+	{
+		queue->tail = queue->head = newNode;
+	}
+	else
+	{
+		queue->tail->next = newNode;
+		queue->tail = newNode;
+	}
+	queue->size++;
+	return (1);
+}
+
+
+
+/**
+ * popFromQueue - program that removes and retrieves a vertex from the front of the queue
+ *
+ * this function removes the node at the front of the queue, retrieves the vertex and its depth,
+ * and then frees the memory of the node;
+ * it adjusts the head of the queue and ensures the correct management of the queue's size
+ *
+ * @queue: a pointer to the 'GraphQueue' from which the vertex will be removed
+ * @vertex: a pointer to store the removed vertex
+ * @depth: a pointer to store the depth of the removed vertex
+ *
+ * Return: 1 if the vertex is successfully removed, 0 if the queue is empty
+ */
+
+int popFromQueue(GraphQueue *queue, vertex_t **vertex, size_t *depth)
+{
+	GraphQueueNode *frontNode;
+
+	if (!queue || !queue->head)
 		return (0);
 
-	visited_vertices = calloc(graph->nb_vertices, sizeof(char));
-	vertex_queue = calloc(graph->nb_vertices + 1, sizeof(vertex_t *));
-	vertex_queue[0] = graph->vertices;
-	visited_vertices[0] = 1;
-	depth = 0;
-	queue_front = 0;
-	current_round_size = 1;
-	next_round_size = 0;
-	for (vertex_index = 0; vertex_queue[vertex_index]; vertex_index++)
+	frontNode = queue->head;
+	queue->head = queue->head->next;
+
+	if (!queue->head)
+		queue->tail = NULL;
+
+	*vertex = frontNode->vertex;
+	*depth = frontNode->depth;
+	queue->size--;
+	free(frontNode);
+
+	return (1);
+}
+
+
+
+/**
+ * breadth_first_traverse - program that performs a breadth-first search (BFS) traversal of a graph
+ *
+ * this function implements the BFS algorithm for graph traversal;
+ * it initializes a queue to manage the vertices to be visited and an array to track visited vertices;
+ * it iteratively visits vertices in a breadth-first manner, applying the provided action function to each
+ * vertex and tracking the maximum depth reached;
+ * the function handles memory allocation for traversal management and ensures cleanup
+ *
+ * @graph: Pointer to the graph structure to be traversed
+ * @action: Function pointer to the action to be performed on each visited vertex
+ *
+ * Return: the maximum depth reached in the traversal, or 0 on failure
+ */
+
+
+size_t breadth_first_traverse(const graph_t *graph, void (*action)(const vertex_t *v, size_t depth))
+{
+	GraphQueue *queue;
+	vertex_t *currentVertex;
+	edge_t *currentEdge;
+	size_t maxDepth = 0, currentDepth, i, queueSize;
+	size_t *visited;
+
+	if (!graph || !action || !graph->nb_vertices)
+		return (0);
+
+	visited = calloc(graph->nb_vertices, sizeof(size_t));
+	if (!visited)
+		return (0);
+
+	queue = calloc(1, sizeof(GraphQueue));
+
+	if (!queue)
 	{
-		vertex_action(vertex_queue[vertex_index], depth);
-		for (edge = vertex_queue[vertex_index]->edges; edge; edge =
-			     edge->next)
+		free(visited);
+		return (0);
+	}
+
+	visited[graph->vertices->index] = 1;
+	pushToQueue(queue, graph->vertices, 0);
+
+	while (queue->size)
+	{
+		queueSize = queue->size;
+		for (i = 0; i < queueSize; i++)
 		{
-			if (visited_vertices[edge->dest->index] == 0)
+			popFromQueue(queue, &currentVertex, &currentDepth);
+			action(currentVertex, currentDepth);
+			maxDepth = (maxDepth > currentDepth) ? maxDepth : currentDepth;
+
+			for (currentEdge = currentVertex->edges; currentEdge; currentEdge = currentEdge->next)
 			{
-				vertex_queue[queue_front++] = edge->dest;
-				visited_vertices[edge->dest->index] = 1;
-				next_round_size += 1;
+				if (!visited[currentEdge->dest->index])
+				{
+					visited[currentEdge->dest->index] = 1;
+					pushToQueue(queue, currentEdge->dest, currentDepth + 1);
+				}
 			}
 		}
-		if (--current_round_size == 0)
-		{
-			depth++;
-			current_round_size = next_round_size;
-			next_round_size = 0;
-		}
 	}
-	free(visited_vertices);
-	free(vertex_queue);
-	return (depth);
+
+	free(visited);
+	freeQueue(queue);
+	return (maxDepth);
 }
