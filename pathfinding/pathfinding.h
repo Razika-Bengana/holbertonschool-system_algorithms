f840b419cbc4cd366ea1d0b11264c38c609fc357
RazikaBengana <razikabengana@gmail.com>
2024-01-14 15:45:49 +0100

    Update header files

--- pathfinding/pathfinding.h
+++ pathfinding/pathfinding.h
@@ -2,16 +2,19 @@
 #define PATHFINDING_H
 
 
-#include "queues.h"
 #include "graphs.h"
+#include "queues.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
+
 
 #define RIGHT {1, 0}
 #define BOTTOM {0, 1}
 #define LEFT {-1, 0}
 #define TOP {0, -1}
+
 
 /**
  * struct point_s - Structure storing coordinates
@@ -27,6 +30,7 @@ typedef struct point_s
 } point_t;
 
 
+
 /* task 0 */
 queue_t *backtracking_array(char **map, int rows, int cols,
 			    point_t const *start, point_t const *target);
@@ -34,11 +38,22 @@ int point_push(queue_t **queue, int x, int y);
 int backtracker(queue_t **queue, int *saw, char **map, int rows,
 		int cols, int x, int y, point_t const *target);
 
+
 /* task 1 */
 queue_node_t *graph_fill(queue_t *new_node, int *saw, const vertex_t *current,
 			 char *target);
 queue_t *backtracking_graph(graph_t *graph, vertex_t const *start,
 			    vertex_t const *target);
+
+
+/* task 2 */
+
+queue_t *dijkstra_graph(graph_t *graph, vertex_t const *start, vertex_t const *target);
+void print_visited_vertex(char *vertex_content);
+void enqueue(queue_t *queue, void *ptr);
+void *dequeue(queue_t *queue);
+int is_empty(queue_t *queue);
+
 
 
 #endif /* PATHFINDING_H */
